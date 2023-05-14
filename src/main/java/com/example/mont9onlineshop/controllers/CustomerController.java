e1cf95998058eb6cb605d48c1703b3c753fc011f
Artur Aidarov <artur230704@gmail.com>
2023-05-14 15:53:18 +0600

    registration and exception
    added registration and global exception handler

--- src/main/java/com/example/mont9onlineshop/controllers/CustomerController.java
+++ src/main/java/com/example/mont9onlineshop/controllers/CustomerController.java
@@ -1,20 +1,37 @@
 package com.example.mont9onlineshop.controllers;
 
 import com.example.mont9onlineshop.DTO.customer.CustomerDTO;
+import com.example.mont9onlineshop.DTO.customer.CustomerRegisterDTO;
 import com.example.mont9onlineshop.services.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
+
+    @GetMapping("/signup")
+    public String getRegistrationPage(Model model){
+        model.addAttribute("message", "");
+        return "registration";
+    }
+
+    @PostMapping(value = "/signup")
+    public HttpEntity<Boolean> register(@Valid @RequestBody CustomerRegisterDTO customer){
+        return new ResponseEntity<>(customerService.register(customer), HttpStatus.OK);
+    }
 
     @GetMapping(value = "/api/customers")
     public ResponseEntity<List<CustomerDTO>> findAll(){
---
+++ src/main/java/com/example/mont9onlineshop/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.mont9onlineshop.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors().stream()
+                .map(e -> String.format("%s - %s",
+                        e.getField(), e.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return  ResponseEntity.unprocessableEntity().body(errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.unprocessableEntity().body(ex.getMessage());
+    }
+}
