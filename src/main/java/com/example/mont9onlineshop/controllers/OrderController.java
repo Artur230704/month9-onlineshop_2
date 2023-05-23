562e308a6fa075b739c7aae8673886c3b5561841
Artur Aidarov <artur230704@gmail.com>
2023-05-23 15:58:29 +0600

    orders
    added orders page

--- src/main/java/com/example/mont9onlineshop/controllers/OrderController.java
+++ src/main/java/com/example/mont9onlineshop/controllers/OrderController.java
@@ -1,29 +1,29 @@
 package com.example.mont9onlineshop.controllers;
 
 import com.example.mont9onlineshop.DTO.order.OrderAddingDTO;
-import com.example.mont9onlineshop.DTO.order.OrderDTO;
 import com.example.mont9onlineshop.services.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
 
-    @GetMapping("/api/orders")
-    public ResponseEntity<List<OrderDTO>> findCustomerOrders(Principal principal){
+    @GetMapping("/orders")
+    public String getOrdersPage(Model model, Principal principal){
         String email = principal.getName();
-        return new ResponseEntity<>(orderService.findCustomerOrders(email),HttpStatus.OK);
+        model.addAttribute("orders",orderService.findCustomerOrders(email));
+        return "orders";
     }
 
     @PostMapping("/api/orders/add")
